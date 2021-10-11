08ba1f612564fbb96f13f27facd160fd294f534e
Micaela <mica.a.korec@gmail.com>
2021-10-10 22:02:05 -0300

    modificacion de nombres

--- src/main/java/InterfacesGRaficas/MenuOpciones.java
+++ src/main/java/InterfacesGRaficas/MenuOpciones.java
@@ -9,12 +9,12 @@ package InterfacesGRaficas;
  *
  * @author micaelakorec
  */
-public class MenuAlumnos extends javax.swing.JFrame {
+public class MenuOpciones extends javax.swing.JFrame {
 
     /**
      * Creates new form MenuAlumnos
      */
-    public MenuAlumnos() {
+    public MenuOpciones() {
         initComponents();
     }
 
@@ -191,20 +191,21 @@ public class MenuAlumnos extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MenuAlumnos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MenuOpciones.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MenuAlumnos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MenuOpciones.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MenuAlumnos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MenuOpciones.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MenuAlumnos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MenuOpciones.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new MenuAlumnos().setVisible(true);
+                new MenuOpciones().setVisible(true);
             }
         });
     }
